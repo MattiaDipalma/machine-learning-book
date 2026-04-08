0a7508987faa60ce001222988ff4e409271375c0
mdipalma <mdipalma@student.ethz.ch>
2026-04-08 23:59:23 +0200

    with gradient the linear regression function is more precised

--- gradient.py
+++ gradient.py
@@ -1,1 +1,27 @@
-print("test: commit gradient")
+import numpy as np
+
+def predict(X,w,b):
+    return X*w+b
+
+def loss(X,Y,w,b):
+    return np.average((predict(X,w,b)-Y)**2)
+
+def gradient(X, Y, w, b):
+    w_gradient = 2 * np.average(X * (predict(X, w, b) - Y))
+    b_gradient = 2 * np.average(predict(X, w, b) - Y)
+    return (w_gradient, b_gradient)
+
+def train(X, Y, iterations, lr):
+    w = b = 0
+    for i in range(iterations):
+        print("Iteration %4d => Loss: %.10f" % (i, loss(X, Y, w, 0)))
+        print("Iteration %4d => Loss: %.10f" % (i, loss(X, Y, w, b)))
+        w_gradient, b_gradient = gradient(X, Y, w, b)
+        w -= w_gradient * lr
+        b -= b_gradient * lr 
+    return w, b
+
+X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
+w, b = train(X, Y, iterations=10000, lr=0.001) 
+print(f"\nw={w}, b={b}")
+print(f"Prediction: x={20} => y={predict(20, w, b)}")
